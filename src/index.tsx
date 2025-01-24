558b908871435980ab689d7213154b3ae2c2e9f9
Максим Смирнов <i23s0038@students.it-college.ru>
2025-01-24 16:55:26 +0500

    gitignore dev | prod

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style.scss';
 import {App} from './app';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
