de95a048fddcf1623f625d62d33fa0465b02ac80
Максим Смирнов <i23s0038@students.it-college.ru>
2025-02-13 11:40:06 +0500

    proc

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import './styles/style.scss';
 import { BrowserRouter as Router, Route, Routes } from 'react-router';
 import UserPage from './pages/UserPage';
@@ -16,16 +15,4 @@ export const App = () => {
       </Routes>
     </Router>
   );
-=======
-import Аdvertisement from './pages/adminPage/components/Advertisement';
-
-export const App = () => {
-  return(
-    <div className='App'>
-      <h1>Hello, React with TypeScript!</h1>
-      <Аdvertisement />
-    </div>
-  )
->>>>>>> 08c08570ff38da3501e1efa9e049cc8ec2f39802
-};
-
+}
